bba493a7ef37e432fbdeedcc83bede55a0a3de4b
Feltesse <er.feltesse@gmail.com>
2020-06-09 11:43:36 +0200

    operationnal code

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,7 +1,13 @@
 const assert = require('assert');
-const capitalizeFirstLetters = require('../capitalizeFirstLetters');
 
 
+function capitalizeFirstLetters(input) {
+  let splitInput = input.toLowerCase().split(' ');
+    for (let i = 0; i < splitInput.length; i++) {
+        splitInput[i] = splitInput[i].charAt(0).toUpperCase() + splitInput[i].substring(1);     
+    }
+    return splitInput.join(' '); 
+ }
  
  
 
@@ -12,7 +18,7 @@ assert.strictEqual(capitalizeFirstLetters.length, 1);
 // Check that capitalizeFirst transforms javaScript rocks correctly
 assert.strictEqual(capitalizeFirstLetters('javascript rocks'), 'Javascript Rocks');
 // Check that it works for a 1-word string
-assert.strictEqual(capitalizeFirstLetters('javascript', 'Javascript'))
+assert.strictEqual(capitalizeFirstLetters('javascript'), 'Javascript')
 // Check that it works for a 1-character string
 assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
 // Check that it works for an empty string
