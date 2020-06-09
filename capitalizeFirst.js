b643a0603d2b0e258a5c72c017726ae3a7d76e3a
Feltesse <er.feltesse@gmail.com>
2020-06-09 11:48:45 +0200

    adding file capitalizeFirst

---
+++ capitalizeFirst.js
@@ -0,0 +1,17 @@
+// capitalizeFirst.js
+const assert = require('assert');
+
+function capitalizeFirst(input) {
+    return input[0].toUpperCase() + input.slice(1);
+  }
+
+// Check that capitalizeFirst is a function
+assert.strictEqual(typeof capitalizeFirst, 'function');
+// Check that capitalizeFirst accepts one argument
+assert.strictEqual(capitalizeFirst.length, 1);
+// Check that capitalizeFirst transforms javaScript correctly
+assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirst('z'), 'Z');
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirst(''), '');
